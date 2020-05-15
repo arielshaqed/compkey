71c8f105e373a55112fe72360ebb67db48e15bfb
Ariel Shaqed (Scolnicov) <ariels@reshuffle.com>
2020-05-15 15:51:22 +0300

    Initial version with get, set
    `Map`-like for `get` and `set` methods.  Handles composite keys by
    "deep-equals" or "insertion-order" comparisons.  Support keys that
    are strings or symbols (TypeScript doesn't have support for symbol
    keys, cast around that).

---
+++ lib/ckey.ts
@@ -0,0 +1,146 @@
+// A Map-like with support for compound keys.
+//
+// Compound keys appear like JavaScript objects and arrays, with deep
+// equality: values matter but insertion order only optionally.
+
+// Equality-comparable JavaScript values
+export type Scalar = number | string | symbol | undefined | null;
+
+// Usable key values in JavaScript objects.  number is cool in Arrays,
+// but actually treated as a *string* object key.
+export type Key = number | string | symbol;
+
+// "Not present", as opposed to "present but undefined".
+const notPresent = Symbol('[not present]');
+
+// Stored for partial (and complete) compound keys.
+interface Node<V> {
+  value: V | typeof notPresent; // value for key here
+
+  next?: Map<string | symbol /* key to use */, Map<Key /* value on that key */, Node<V>>>;
+}
+
+enum Rank {
+  UNDEFINED = 0,
+  NULL = 10,
+  NUMBER = 20,
+  STRING = 30,
+  SYMBOL = 40,
+  OTHER = Infinity,
+}
+
+function rankScalarType(a: Scalar): Rank {
+  switch (a) {
+    case undefined: return Rank.UNDEFINED;
+    case null: return Rank.NULL;
+    default:
+      switch(typeof a) {
+        case 'number': return Rank.NUMBER;
+        case 'string': return Rank.STRING;
+        case 'symbol': return Rank.SYMBOL;
+        default: return Rank.OTHER; // May fail elsewhere, of course.
+      }
+  }
+}
+
+export function compareScalars(a: Scalar, b: Scalar): number {
+  // If a and b have different ranks, compare those ranks.
+  const rank = rankScalarType(a);
+  const compareRanks = rank - rankScalarType(b);
+  if (compareRanks !== 0) return compareRanks;
+
+  switch (rank) {
+    case Rank.UNDEFINED: case Rank.NULL: return 0;
+    case Rank.NUMBER: return (a as number) - (b as number);
+    case Rank.STRING:
+      return (a as string) < (b as string) ? -1 : (a as string) > (b as string) ? +1 : 0;
+    case Rank.SYMBOL: {
+      const as = a!.toString();
+      const bs = b!.toString();
+      return as < bs ? -1 : as > bs ? +1 : 0;
+    }
+    case Rank.OTHER:
+      throw new Error(`Non-scalar values`);
+    default:
+      throw new Error(`Internal error: rank ${rank} not covered`);
+  }
+}
+
+type KeySequence = IterableIterator<string | symbol> | Array<string | symbol>;
+
+// Keys generator matching "JavaScript object" behaviour: all keys
+// contain (recursively) matching values, in (almost) the same
+// insertion order.  This is *not* deep equality.
+//
+// This is "*almost*" insertion order: symbol keys come after string
+// keys because JavaScript.
+export function asObjects<K>(obj: K): Array<symbol | string> {
+  return (Object.getOwnPropertyNames(obj) as Array<symbol | string>)
+    .concat(Object.getOwnPropertySymbols(obj))
+    .filter((key) => Object.getOwnPropertyDescriptor(obj, key)!.enumerable);
+}
+
+// Keys generator matching "deep equals" behaviour: all keys contain
+// (recursively) matching values, ignoring insertion order.  This
+// sort-of works for arrays, too, but does not return indices in
+// ascending numerical order.
+export function asValues<K>(obj: K): Array<symbol | string> {
+  return asObjects(obj).sort(compareScalars);
+}
+
+// TODO(ariels): Key generator for arrays.
+
+function getOrMaybeAdd<A, B>(map: Map<A, B>, a: A, makeEl: () => B): B {
+  if (map.has(a)) return map.get(a)!;
+  const el = makeEl();
+  map.set(a, el);
+  return el;
+}
+
+export class CMap<K extends Record<string | symbol, any>, V> {
+  // The type Record<string | symbol, any> is a lie: the TypeScript
+  // type system has no support for symbol keys; support it anyway by
+  // casting all keys to string -- even then they are symbols.
+  private values: Node<V> = { value: notPresent };
+
+  constructor(private readonly keyGenerator: (v: K) => KeySequence = asValues) {}
+
+  // Returns a node for key or null if none exists.
+  private findNode(key: K): Node<V> | null {
+    const keySeq = this.keyGenerator(key);
+    let node: Node<V> | undefined = this.values;
+    for (const keyElement of keySeq) {
+      node = node.next?.get(keyElement)?.get(key[keyElement as string]);
+      if (!node) return null;
+    }
+    return node;
+  }
+
+  // Returns a node for key.  Creates missing parts and always returns
+  // a node if create, returns null if !create and key not found.
+  private makeNode(key: K): Node<V> {
+    const keySeq = this.keyGenerator(key);
+    let node = this.values;
+    for (const keyElement of keySeq) {
+      if (!node.next) node.next = new Map();
+      const forKeyElement = getOrMaybeAdd(node.next, keyElement, () => new Map<Key, Node<V>>());
+      node = getOrMaybeAdd(forKeyElement, key[keyElement as string], () => ({ value: notPresent }));
+    }
+    return node;
+  }
+
+  public get(key: K): V | undefined {
+    const node = this.findNode(key);
+    if (!node || node.value === notPresent) return undefined;
+    return node.value;
+  }
+
+  public set(key: K, value: V): this {
+    const node = this.makeNode(key);
+    node.value = value;
+    return this;
+  }
+
+  // For debugging
+  public _internalDump(): any { return this.values; }
+}
---
+++ test/ckey.spec.ts
@@ -0,0 +1,73 @@
+import { CMap, asObjects, asValues } from '../lib/ckey';
+import test from 'ava';
+import { inspect } from 'util';
+
+function dump<K, V>(map: CMap<K, V>): string {
+  return inspect(map._internalDump(), { depth: null });
+}
+
+test('asObjects returns keys in insertion order, strings before symbols', (t) => {
+  const obj: any = {
+    a: 1,
+    z: { x: 2, y: 2 },
+    c: 3,
+  };
+  t.deepEqual(asObjects(obj), [ 'a', 'z', 'c']);
+
+  const s = Symbol('s');
+  obj[s] = 'symbol';
+  obj['d'] = 4;
+  t.deepEqual(asObjects(obj), [ 'a', 'z', 'c', 'd', s]);
+});
+
+test('asValues returns keys in fixed (sorted) order', (t) => {
+  const obj: any = {
+    a: 1,
+    z: { x: 2, y: 2 },
+    c: 3,
+  };
+  t.deepEqual(asValues(obj), [ 'a', 'c', 'z']);
+
+  const s = Symbol('s');
+  obj[s] = 'symbol';
+  t.deepEqual(asValues(obj), [ 'a', 'c', 'z', s ]);
+});
+
+test('CMap with deep-equal keys', (t) => {
+  const map = new CMap();
+
+  map.set({ a: 1, b: 2 }, 12)
+    .set({ a: 2, b: 1 }, 21)
+    .set({ a: 1, b: 1 }, 11);
+
+  t.is(map.get({ a: 1, b: 2 }), 12, `map internals: ${dump(map)}`);
+  t.is(map.get({ b: 2, a: 1 }), 12, `map internals: ${dump(map)}`);
+  t.is(map.get({ a: 2, b: 1 }), 21, `map internals: ${dump(map)}`);
+  t.is(map.get({ a: 2, b: 9 }), undefined, `map internals: ${dump(map)}`);
+  t.is(map.get({ a: 1, b: 2, c: 3 }), undefined, `map internals: ${dump(map)}`);
+  t.is(map.get({ a: 1 }), undefined, `map internals: ${dump(map)}`);
+});
+
+test('CMap with deep-equal keys and symbols', (t) => {
+  const map = new CMap();
+  const s = Symbol('s');
+
+  map.set({ [s]: 0, a: 1, b: 2 }, 1);
+  map.set({ [s]: 1, a: 1, b: 2 }, 2);
+  t.is(map.get({ a: 1, b: 2, [s]: 1 }), 2);
+  t.is(map.get({ a: 1, [s]: 0, b: 2 }), 1);
+});
+
+test('CMap with insertion-order keys', (t) => {
+  const map = new CMap(asObjects);
+
+  map.set({ a: 1, b: 2 }, 12)
+    .set({ b: 2, a: 1 }, '12')
+    .set({ a: 1, b: 3 }, 13)
+    .set({ a: 1 }, 1);
+
+  t.is(map.get({ a: 1, b: 2 }), 12);
+  t.is(map.get({ b: 2, a: 1 }), '12');
+  t.is(map.get({ a: 1, b: 3 }), 13);
+  t.is(map.get({ a: 1 }), 1);
+});
